63ecfcf3a0cd36b1855f2a03e99bb38262921290
Skryba <mmstarkillerm@gmail.com>
2021-04-16 13:16:01 +0200

    LocalStorage.billList dodawanie i usuwanie dziala

--- paragon.js
+++ paragon.js
@@ -2,9 +2,9 @@
 @TODO LIST
   - wprowadzanie nowych pozycji paragonu (+1),
   - edycję istniejących pozycji (+1),
-  - usuwanie ich (+1),
+  [done] - usuwanie ich (+1),
   - zmianę kolejności (+1).
-  - local storage - Zawartość listy przechowuj (+1)
+  [done] - local storage - Zawartość listy przechowuj (+1) 
 */
 /*
 Zalecane podejście: 
@@ -15,100 +15,94 @@ Manipulacja pozycjami powoduje
 zarówno aktualizację przechowywanego w pamięci modelu, 
 jak i modyfikację widoku (fragmentu dokumentu).*/
 
-///*@TODO 
+///*@TODO
 // 1.Ogarnąć dodawanie elementów przez formularz
 // 2.dodawanie elementów na localStorage
 
-
 class Product {
   constructor(name, quantity, price) {
-    this.name= name;
-    this.quantity=quantity;
-    this.price=price;
+    this.name = name;
+    this.quantity = quantity;
+    this.price = price;
   }
-
 }
 
-class Bills{
-    constructor(){
-        this.productList  =[];
-    }
+class Bills {
+  constructor() {
+    this.productList = [];
+  }
 }
 
 console.log("działam");
 ///*@TODO ogarnięcie formularza
 
-
 let testBill = new Bills();
-let testProduct1 = new Product("Mango",2,10);
-let testProduct2 = new Product("Jabłko",3,8);
-let testProduct3 = new Product("Xiaomi Lepsze",2,3000);
-testBill.productList.push(testProduct1);
-testBill.productList.push(testProduct2);
-testBill.productList.push(testProduct3);
-testBill.productList.push(testProduct3);
-testBill.productList.push(testProduct3);
-testBill.productList.push(testProduct3);
-testBill.productList.push(testProduct3);
-testBill.productList.push(testProduct3);
-console.log(testBill.productList);
 
 const table = document.getElementById("billTable");
 
-
-//starczy dodawac na stringa
-
-//dodawanie elementu do rachunku 
-const addBill = (id,val) =>{
+//dodawanie elementu do html
+const addBill = (id, val) => {
   let row = table.insertRow();
   let lp = row.insertCell(0);
   let nazwa = row.insertCell(1);
   let ilosc = row.insertCell(2);
   let cena = row.insertCell(3);
   let suma = row.insertCell(4);
-  lp.id="id";
-  lp.innerHTML = id+1;
-  nazwa.innerHTML =val.name;
-  ilosc.innerHTML =val.quantity;
-  cena.innerHTML =val.price;
+  let usun = row.insertCell(5);
+  lp.id = "id";
+  lp.innerHTML = id + 1;
+  nazwa.innerHTML = val.name;
+  ilosc.innerHTML = val.quantity;
+  cena.innerHTML = val.price;
   suma.innerHTML = val.quantity * val.price;
+  usun.innerHTML = "<button onclick='deleteProduct(this)'>Usuń</button>";
+};
+
+
+function deleteProduct(x){
+  var i = x.parentNode.parentNode.rowIndex;
+  document.getElementById("billTable").deleteRow(i);
+  // console.log(i);
+ testBill.productList.splice(i-1,1);
+  // console.log(testBill.productList);
+  localStorage.billList=JSON.stringify(testBill.productList);
 }
 
-
-testBill.productList.forEach((val,id)=>
- addBill(id,val)
-);
-
+//dodawanie nowego rachunku
 const addForm = document.getElementById("addForm");
 console.log(addForm.name);
-addForm.onsubmit = (event) => 
-{
-  let name= addForm.name.value;
-  let quantity= addForm.quantity.value;
-  let cost= addForm.cost.value;
-  let prod=new Product(name,quantity,cost);
+addForm.onsubmit = (event) => {
+  let name = addForm.name.value;
+  let quantity = addForm.quantity.value;
+  let cost = addForm.cost.value;
+
+  let prod = new Product(name, quantity, cost);
+
   testBill.productList.push(prod);
-  addBill(testBill.productList.length-1,prod);
+  addBill(testBill.productList.length - 1, prod);
+
+  localStorage.billList = JSON.stringify(testBill);
   event.preventDefault();
-}
+};
 
-// let produkty = [];
-
-// localStorage.produkty = testBill.productList;
-// console.log(produkty);
-
-// testBill.productList = localStorage.produkty;
-
+// wczytywanie elementów przy uruchomieniu
+document.body.onload = function () {
+  if (localStorage.billList) 
+  {
+    testBill = JSON.parse(localStorage.billList);
+    console.log(testBill);
+    testBill.productList.forEach((val, id) => addBill(id, val));
+  }
+};
 
 //sortowanie tablicy g4g
-function sortTableASC(){
+function sortTableASC() {
   var rows, switching, i, x, y, shouldSwitch;
   switching = true;
-  while (switching) 
-  {
+  while (switching) {
     switching = false;
     rows = table.rows;
-    for (i = 1; i < (rows.length - 1); i++) {
+    for (i = 1; i < rows.length - 1; i++) {
       shouldSwitch = false;
       x = rows[i].getElementsByTagName("TD")[0];
       y = rows[i + 1].getElementsByTagName("TD")[0];
@@ -122,11 +116,9 @@ function sortTableASC(){
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
     }
-  }  
+  }
 }
 sortTableASC();
-
-
 
 // console.log(table.rows);
 // console.log(table.rows[0].cells.length);
