59ddf146070deac384e077ee261992f7afcf4cdc
Skryba <mmstarkillerm@gmail.com>
2021-04-20 21:37:02 +0200

    przesuwanie nie dziala

--- paragon.js
+++ paragon.js
@@ -5,20 +5,15 @@
   [done] - usuwanie ich (+1),
   - zmianę kolejności (+1).
   [done] - local storage - Zawartość listy przechowuj (+1) 
-  - suma.
+  [done] - suma.
+  - walidacja
+  - zmiennoprzecinkowe wartości.
 */
 /*
-Zalecane podejście: 
-przechowujcie reprezentację paragonu w postaci tablicy obiektów, 
-a dopiero na tej podstawie budujcie dokument. 
-
 Manipulacja pozycjami powoduje 
 zarówno aktualizację przechowywanego w pamięci modelu, 
 jak i modyfikację widoku (fragmentu dokumentu).*/
 
-///*@TODO
-// 1.Ogarnąć dodawanie elementów przez formularz
-// 2.dodawanie elementów na localStorage
 let productList = [];
 
 class Product {
@@ -30,10 +25,20 @@ class Product {
 }
 
 const table = document.getElementById("billTable");
-
+let id;
 //dodawanie elementu do html
 const addBill = (id, val) => {
   let row = table.insertRow();
+  row.className = "product";
+
+  // row.addEventListener('mousedown', function (event)
+  // {
+  //   if(selected!==this.rowIndex){
+  //     event.target.parentNode.style.backgroundColor = "orange";
+  //     selected=this.rowIndex;
+  //   }
+  // });
+
   let lp = row.insertCell(0);
   let nazwa = row.insertCell(1);
   let ilosc = row.insertCell(2);
@@ -41,40 +46,84 @@ const addBill = (id, val) => {
   let suma = row.insertCell(4);
   let usun = row.insertCell(5);
   let edytuj = row.insertCell(6);
+  let arrows = row.insertCell(7);
+  // let upArrow = row.insertCell(7);
+  // let downArrow = row.insertCell(7);
   lp.id = "id";
   lp.innerHTML = id + 1;
   nazwa.innerHTML = val.name;
   ilosc.innerHTML = val.quantity;
   cena.innerHTML = val.price;
-  suma.innerHTML = val.quantity * val.price;
+  suma.innerHTML = Math.round(val.quantity * val.price * 100) / 100;
   usun.innerHTML = "<button onclick='deleteProduct(this)'>Usuń</button>";
   edytuj.innerHTML = "<button onclick='editProduct(this)'>Edytuj</button>";
-  // console.log(table.rows.length);
+
+  let upArrow = document.createElement("DIV");
+  upArrow.className = "material-icons arrow";
+  upArrow.innerText = "arrow_upward";
+  upArrow.style["height"] = "10px";
+  
+
+  let downArrow = document.createElement("DIV");
+  downArrow.className = "material-icons arrow";
+  downArrow.innerText = "arrow_downward";
+
+  let allinone = document.createElement("DIV");
+  allinone.appendChild(upArrow);
+  allinone.appendChild(downArrow);
+  arrows.appendChild(allinone);
+
+  upArrow.addEventListener(
+    "mousedown",
+     (event)=>
+     {
+       let currRow=event.target.parentNode.parentNode.parentNode.rowIndex;
+      console.log(currRow);
+      let id=currRow;
+      if (id > 1) {
+        table.rows[id].parentNode.insertBefore(table.rows[id], table.rows[id - 1]);
+      }
+     } 
+  );
+  downArrow.addEventListener(
+    "mousedown",
+     (event)=>
+     {
+       let currRow=event.target.parentNode.parentNode.parentNode.rowIndex;
+      console.log(currRow);
+      let id=currRow;
+      console.log(table.rows.length);
+      if (id < table.rows.length - 1) {
+        table.rows[id].parentNode.insertBefore(table.rows[id + 1], table.rows[id]);
+      }
+     } 
+  );
 };
 
-
-function deleteProduct(x){
+function deleteProduct(x) {
   var i = x.parentNode.parentNode.rowIndex;
   document.getElementById("billTable").deleteRow(i);
-  productList.splice(i-1,1);
+  productList.splice(i - 1, 1);
   numerize(productList.length);
-  localStorage.billList=JSON.stringify(productList);
+  localStorage.billList = JSON.stringify(productList);
+  suma();
 }
 
-function editProduct(x){
+function editProduct(x) {
   var i = x.parentNode.parentNode.rowIndex;
-  var obiekt = productList[i-1];
+  var obiekt = productList[i - 1];
   var editQuantity = prompt("Ile chcesz mieć powtórzeń tego produktu?");
-  if(editQuantity > 0 && !isNaN(editQuantity)){
+  if (editQuantity > 0 && !isNaN(editQuantity)) {
     obiekt.quantity = editQuantity;
     var table = document.getElementById("billTable");
     table.rows[i].cells[2].innerHTML = obiekt.quantity;
     var suma = obiekt.quantity * obiekt.price;
     table.rows[i].cells[4].innerHTML = suma;
     localStorage.billList = JSON.stringify(productList);
-  }else{
+  } else {
     alert("Podana przez ciebie liczba jest niewłaściwa!");
   }
+  this.suma();
 }
 
 //dodawanie nowego rachunku
@@ -89,54 +138,67 @@ addForm.onsubmit = (event) => {
 
   localStorage.billList = JSON.stringify(productList);
   event.preventDefault();
+  suma();
 };
 
 // wczytywanie elementów przy uruchomieniu
 document.body.onload = function () {
-  if (localStorage.billList ) 
-  {
+  if (localStorage.billList) {
     productList = JSON.parse(localStorage.billList);
     console.log(productList);
     console.log(JSON.parse(localStorage.billList));
-    productList.forEach((val, id) => addBill(id, val));    
+    productList.forEach((val, id) => addBill(id, val));
+    suma();
   }
-  
 };
 
-//sortowanie tablicy g4g
-function sortTableASC() {
-  var rows, switching, i, x, y, shouldSwitch;
-  switching = true;
-  while (switching) {
-    switching = false;
-    rows = table.rows;
-    for (let i = 1; i < rows.length - 1; i++) {
-      shouldSwitch = false;
-      x = rows[i].getElementsByTagName("TD")[0];
-      y = rows[i + 1].getElementsByTagName("TD")[0];
-
-      if (Number(x.innerHTML) > Number(y.innerHTML)) {
-        shouldSwitch = true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-  }
-}
 // console.log(document.getElementById("billTable"));
 // console.log(document.getElementById("billTable").rows[1].cells.length);
-console.log(table.rows[0].cells.length);
+// console.log(table.rows[0].cells.length);
 
-//numerowanie nie dziala
+function suma() {
+  productList = JSON.parse(localStorage.billList);
+  var suma = 0;
+  console.log(`Dlugosc listy: ${productList.length}`);
+  if (productList.length == 0) {
+    suma = 0;
+  } else {
+    for (let j = 0; j < productList.length; j++) {
+      suma += productList[j].price * productList[j].quantity;
+    }
+  }
+  let total = document.getElementById("total");
+  total.style.margin = "0 auto";
+  total.innerHTML = `Łącznie: ${suma}`;
+  total.style.fontSize = "1.5em";
+  total.style.marginBottom = "20px";
 
-function numerize(tableLength){
-  // for(let rowId=1;rowId<=table.rows.length;rowId++)
-  // console.log(rowId);
-  // console.log(table.rows[rowId].cells.length);
-  for(let j=1;j<=tableLength;j++){
+  console.log(suma);
+}
+
+function numerize(tableLength) {
+  for (let j = 1; j <= tableLength; j++) {
     table.rows[j].cells[0].innerHTML = j;
+  }
+}
+
+
+
+function upDown(dir) {
+  let rows = table.rows;
+
+  if (dir === "up") {
+    if (id > 1) {
+      table.insertBefore(rows[id], rows[id - 1]);
+      id--;
+    }
+  }
+
+  if (dir === "down") {
+    if (id < rows.length - 1) {
+      table.insertBefore(rows[id + 1], rows[id]);
+      // when the row go down the id will be equal to id + 1
+      id++;
+    }
   }
 }
